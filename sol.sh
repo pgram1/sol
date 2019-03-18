e03b3da18eacd139c3a512efb3596c971c703f2f
Damian96 <damgiankakis@gmail.com>
2019-03-18 11:57:20 +0200

    Continues

--- sol.sh
+++ sol.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ART="./ascii"
-COLS=$(tput cols)
+# ART="./ascii"
+# COLS=$(tput cols)
 
-command -v cat > /dev/null 2>&1 || { printf "\n%s\n" "Cat is required, and it isn't installed."; }
+# command -v cat > /dev/null 2>&1 || { printf "\n%s\n" "Cat is required, and it isn't installed."; }
 
 EARTH=$( cat ./ascii/earth.txt )
 
-printf "%s" $EARTH
+# printf "%s" $EARTH
+cat $EARTH
 
 # printf "%*s\n" $(((${#EARTH}+$COLS)/2)) "$EARTH"
 
